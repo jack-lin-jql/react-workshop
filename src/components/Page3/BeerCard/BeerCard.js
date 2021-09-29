23cf794250b752a2053c4e529474f716c3991d91
jack-lin-jql <jack.lin.jql@gmail.com>
2021-09-28 23:47:13 -0400

    Minor refactor

--- src/components/Page3/BeerCard/BeerCard.js
+++ src/components/Page3/BeerCard/BeerCard.js
@@ -1,7 +1,15 @@
 export const BeerCard = ({ beer }) => {
-  return <div style={{ width: "300px", display: "inline-block" }}>
-    <img src={beer.image_url} alt={beer.name} height="300" />
-    <div><b>Name: </b>{beer.name}</div>
-    <div><b>Description: </b>{beer.description}</div>
-  </div>
+  return (
+    <div style={{ width: "300px" }}>
+      <img src={beer.image_url} alt={beer.name} height="300" />
+      <div>
+        <b>Name: </b>
+        {beer.name}
+      </div>
+      <div>
+        <b>Description: </b>
+        {beer.description}
+      </div>
+    </div>
+  );
 };
--- src/components/Page3/Page3.js
+++ src/components/Page3/Page3.js
@@ -17,17 +17,16 @@ export const Page3 = (props) => {
   const beersIn3 = [];
 
   beers.forEach((beer, index) => {
-    if (!(index % 3))
-      beersIn3.push([]);
+    if (!(index % 3)) beersIn3.push([]);
 
-    beersIn3[beersIn3.length - 1].push(beer)
+    beersIn3[beersIn3.length - 1].push(beer);
   });
 
   return (
     <div>
-      {beersIn3.map(threeBeers => (
-        <div>
-          {threeBeers.map(beer => (
+      {beersIn3.map((threeBeers) => (
+        <div style={{ display: "flex" }}>
+          {threeBeers.map((beer) => (
             <BeerCard beer={beer} />
           ))}
         </div>
